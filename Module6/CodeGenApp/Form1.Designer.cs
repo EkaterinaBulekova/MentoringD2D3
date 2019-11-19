1f8fd4bb53ca4862e41454f777fd21cd32b925c5
Yekaterina_Bulekova <yekaterina_bulekova@epam.com>
2019-11-19 10:33:03 +0600

    implemented code generator for TaskApp.exe

---
+++ Module6/CodeGenApp/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CodeGenApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private Button getButton;
+
+        private TextBox resultTextbox;
+
+        private Label resultLabel;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            getButton = new Button();
+            resultTextbox = new TextBox();
+            resultLabel = new Label();
+            SuspendLayout();
+            getButton.Location = new Point(188, 25);
+            getButton.Name = "bt_check";
+            getButton.Size = new Size(55, 20);
+            getButton.TabIndex = 0;
+            getButton.Text = "Get key";
+            getButton.UseVisualStyleBackColor = true;
+            getButton.Click += ev_a;
+            resultTextbox.Location = new Point(35, 25);
+            resultTextbox.Name = "tb_key";
+            resultTextbox.Size = new Size(150, 20);
+            resultTextbox.TabIndex = 1;
+            resultLabel.AutoSize = true;
+            resultLabel.Location = new Point(32, 9);
+            resultLabel.Name = "label1";
+            resultLabel.Size = new Size(107, 13);
+            resultLabel.TabIndex = 2;
+            resultLabel.Text = "Please, press button to get key";
+            base.AutoScaleDimensions = new SizeF(6f, 13f);
+            base.AutoScaleMode = AutoScaleMode.Font;
+            base.ClientSize = new Size(280, 80);
+            base.Controls.Add(resultLabel);
+            base.Controls.Add(resultTextbox);
+            base.Controls.Add(getButton);
+            base.Name = "Form1";
+            Text = "Code generator";
+            ResumeLayout(performLayout: false);
+            PerformLayout();
+            this.components = new System.ComponentModel.Container();
+        }
+
+        #endregion
+    }
+}
+
---
+++ Module6/CodeGenApp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace CodeGenApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ev_a(object A_0, EventArgs A_1)
+        {
+            resultTextbox.Text = GetExpectation();
+        }
+
+        private string GetExpectation()
+        {
+            NetworkInterface netInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+            byte[] addressBytes = netInterface.GetPhysicalAddress().GetAddressBytes();
+            byte[] dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            int[] result = addressBytes.Select((byte adr, int idx) => adr ^ dateBytes[idx]).Select((int num)=>(num <= 999) ? num * 10 : num).ToArray();
+            return string.Join("-", result.Select(num => num.ToString()));
+        }
+    }
+}
+
